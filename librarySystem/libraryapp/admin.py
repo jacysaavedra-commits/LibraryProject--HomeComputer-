98fe47aa97f897d09f21a080ffee5f05ec728ef1
jacysaavedra-commits <jacysaavedra@gmail.com>
2026-04-28 22:23:27 +1200

    sdfasf

--- librarySystem/libraryapp/admin.py
+++ librarySystem/libraryapp/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 from .models import Book, Genre, Customer, BookTransaction, BookReturn
+
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(Genre)
 admin.site.register(Customer)
-admin.site.register(BookTransaction)
 admin.site.register(BookReturn)
+
+
+class BookTransactionAdmin(admin.ModelAdmin):
+    fields = ['customer', 'book', 'issue_date', 'return_date', 'status']
+    readonly_fields = ['return_date']
+    list_display = ['book', 'customer', 'issue_date', 'return_date', 'status']
+
+
+admin.site.register(BookTransaction, BookTransactionAdmin)
